574abad38998a0e0135e976e264a567493bd8770
Fernando Medina Corey <fmcorey@gmail.com>
2018-02-26 08:34:37 -0800

    remove global query from local query file

--- dynamodb/indexes/query_local_index.py
+++ dynamodb/indexes/query_local_index.py
@@ -15,18 +15,3 @@ q_res=dynamodb.query(
     },
     KeyConditionExpression='Artist = :p_key AND price > :s_key'
 )
-
-print(q_res)
-
-print('Query Global Index:')
-q_res=dynamodb.query(
-    TableName="MusicTwo",
-    IndexName="studio-price-index",   
-    Select='ALL_ATTRIBUTES',
-    ReturnConsumedCapacity='TOTAL',
-    ExpressionAttributeValues={
-        ":p_key":{"S":"Anthony Haslett"},
-        ":s_key":{"S":"December Lavender"}
-    },
-    KeyConditionExpression='Artist = :p_key AND SongTitle = :s_key'
-)
