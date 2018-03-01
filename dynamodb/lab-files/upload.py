99d68b81ae49eb0cdaa8a5b5d97410114ef134c9
Fernando Medina Corey <fmcorey@gmail.com>
2018-03-01 12:35:32 -0800

    Add clues

--- dynamodb/lab-files/upload.py
+++ dynamodb/lab-files/upload.py
@@ -1,6 +1,5 @@
 # There are some issues in this code. 
-# Time for you to fix them and get that 
-# data where it needs to go!
+# I left a few comments to help you fix it while I'm on vacation
 
 import boto3
 import json
@@ -12,8 +11,10 @@ def upload():
         songs = json.load(songfile)
     for song in songs:
         response = dynamodb.put_item(
+            # What was the table name? Can't remember.
             TableName='aTable?', 
             Item={
+                # Pretty sure I need to clean up some data types here
                 'Artist':{'A':song['Artist']},
                 'SongTitle':{'S':song['SongTitle']},
                 'price':{'P': song['price']},
@@ -23,4 +24,4 @@ def upload():
         )
         print(response)
 
-the_uploader()
+upload()
