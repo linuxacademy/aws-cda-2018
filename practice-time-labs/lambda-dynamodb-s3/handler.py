3e45383d9b088f228ba2e1989fed4f2c93545416
Fernando Medina Corey <fmcorey@gmail.com>
2018-03-05 13:22:26 -0800

    Fix looping bug, add checks for filetypes, fix dynamodb outputs

--- practice-time-labs/lambda-dynamodb-s3/handler.py
+++ practice-time-labs/lambda-dynamodb-s3/handler.py
@@ -3,11 +3,17 @@ import csv
 import json
 
 s3 = boto3.client('s3')
+dynamodb = boto3.client('dynamodb')
 
 def handler(event, context):
     # Get the object from the event then download it to Lambda tmp space
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
+    # IMPORTANT:
+    if '.csv' not in key:
+        return 'Not a csv that needs processing'
+    if '.psv' in key:
+        return 'An already processed logfile'
     s3.download_file(bucket, key, '/tmp/' + key)
     psv_name = 'processed_' + key[0:-4] + '.psv'
     # Open the file, process it, and upload a psv
@@ -17,5 +23,22 @@ def handler(event, context):
         writer = csv.DictWriter(outfile, reader.fieldnames, delimiter='|')
         writer.writeheader()
         writer.writerows(reader)
-
     s3.upload_file('/tmp/' + psv_name, bucket, psv_name)
+    # Use DynamoDB atomic counters to tally visits in csv
+    with open('/tmp/' + key, 'r') as infile:
+        for row in infile:
+            ddb_key = row.strip().split(',')[0]
+            ddb_inc = row.strip().split(',')[1]
+            if 'site' not in ddb_key:
+                response = dynamodb.update_item(
+                    TableName='siteVisits', 
+                    Key={
+                        'siteUrl':{'S': ddb_key}
+                    },
+                    UpdateExpression='ADD visits :inc',
+                    ExpressionAttributeValues={
+                        ':inc': {'N': ddb_inc}
+                    },
+                    ReturnValues="UPDATED_NEW"
+                )
+                print(response)
